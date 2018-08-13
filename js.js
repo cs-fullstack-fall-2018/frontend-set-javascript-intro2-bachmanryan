f0ee1ebb40eae9724d1c09b00a88541e6e9b653d
Ryan Bachman <jacksprat455@gmail.com>
2018-08-13 12:03:16 -0500

    Frontend_Intro2

--- js.js
+++ js.js
@@ -1,11 +1,24 @@
-var arrayOfNumbers = ["Bob", "Fred", "Josh", "Tosh", "Harry"];
+// var arrayOfNumbers = ["Bob", "Fred", "Josh", "Tosh", "Harry"];
+//
+// var stringOfArray = "";
+// for(var x = 0; x < arrayOfNumbers.length; x++)
+// {
+//     stringOfArray += arrayOfNumbers[x] + ", ";
+// }
+//
+// document.getElementById("something").textContent= stringOfArray;
+
+var newArray = [1,2,3,4,5,6];
 
 var stringOfArray = "";
-for(var x = 0; x < arrayOfNumbers.length; x++)
+for(var x = 0; x < newArray.length; x++)
 {
-    stringOfArray += arrayOfNumbers[x] + ", ";
+    stringOfArray += newArray[x] + ", ";
 }
 
-// document.getElementById("something").textContent= stringOfArray;
-//
-// var newArray = [1,2,3,4,5,6];
+document.getElementById("newHeader").textContent= stringOfArray;
+
+newArray.splice(2,1);
+newArray.splice(3,1);
+
+document.getElementById("newHeaderSpot").textContent=stringOfArray;
